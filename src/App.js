8681f08c70c70e4ba346f4cb126e6631975bb6ee
skj <skj.jo0c@gmail.com>
2017-11-24 16:04:00 +0545

    axios setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import './Main.css';
+import axios from 'axios';
 
 class App extends Component {
   render() {
+	axios.get('http://localhost:3000/advertisements')
+	  .then(function (response) {
+	    console.log(response);
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	  });
     return (
       <div className="App">
         <br/> <br/>
