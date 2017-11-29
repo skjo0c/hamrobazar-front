7d0026a32b93ae8f2743c465bace1983706cdda7
skj <skj.jo0c@gmail.com>
2017-11-29 19:27:06 +0545

    view detail of each ad

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
-
+import yamcha from '../yamcha.png';
+import '../Main.css'
 
 export default class NotFound extends Component{
 	render(){
 		return(
 			<div className = "NotFound">
-				<br/><br/><br/><br/><br/>
-				<h1> NotFound </h1>
+				<div className = "yamcha">
+					<img src = {yamcha} alt = 'yamcha'/>
+					<div className = "yamchaText">
+						<h1> SOMETHING WENT WRONG. </h1>
+					</div>
+				</div>
 			</div>
 		);
 	}
