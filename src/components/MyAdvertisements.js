08233070c6f27d329ee565e2056c90d5e1742614
skj <skj.jo0c@gmail.com>
2017-12-31 14:14:04 +0545

    delete

--- src/components/MyAdvertisements.js
+++ src/components/MyAdvertisements.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
+import {Button} from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -43,10 +44,37 @@ export default class MyAdvertisements extends Component{
 								{advertisement.name}
 							</Link>
 						</h1>
-						 
+						<Button onClick={this.edit_item} id = {advertisement.id} bsStyle = "success" bsSize = "small" > Edit </Button>
+						<Button onClick={this.delete_item} id = {advertisement.id} bsStyle = "danger" bsSize = "small" > Delete </Button>
 					</div>
 				)}
 			</div>
 		)
+	}
+
+	delete_item(event){
+		console.log(event.target.id)
+		let id = event.target.id
+		axios.defaults.headers['Authorization'] = localStorage.getItem('token');
+		axios.delete('http://localhost:3000/api/v1/advertisements/'+id)
+		.then((response)=>{
+			window.location ='/'
+		})
+		.catch(function(error){
+			console.log(error);
+		});
+	}
+
+	edit_item(event){
+		console.log(event.target.id)
+		//here need to open up a BOOTSTRAP MODAL
+		let id = event.target.id
+		// axios.patch('http://localhost:3000/api/v1/advertisements/'+id)
+		// .then((response)=>{
+		// 	window.location ='/'
+		// })
+		// .catch(function(error){
+		// 	console.log(error);
+		// });
 	}
 }
